84a4a58681bdfee1597235ef362bcc3632e24d1c
Yukio-Nishida <142161225+Yukio-Nishida@users.noreply.github.com>
2024-01-17 18:22:44 +0900

    Add files via upload

--- kadai_005/score.php
+++ kadai_005/score.php
@@ -8,6 +8,7 @@
     <p>
     <?php
 
+    //生徒10人のスコアを変数score_1～10に表示
     $score_1 = 80;
 
     $score_2 = 60;
@@ -28,9 +29,14 @@
 
     $score_10 = 60;
     
+    //生徒10人全員分の合計得点を変数score_allに代入
     $score_all = $score_1 + $score_2 + $score_3 + $score_4 + $score_5 + $score_6 + $score_7 + $score_8 + $score_9 + $score_10;
 
-    echo "平均得点は" .($score_all / 10) ."です。";
+    //生徒10人全員分の平均得点を変数score_averageに代入
+    $score_average = $score_all / 10;
+
+    //平均得点をブラウザに出力
+    echo "平均得点は" .$score_average ."です。";
     ?>
     
 </p>
