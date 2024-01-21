a95eb2bf937ced924fc2e6494688fca93704a186
Yukio-Nishida <142161225+Yukio-Nishida@users.noreply.github.com>
2024-01-22 00:09:31 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<title>PHP基礎編</title>
+</head>
+
+<body>
+    
+    <?php
+    // 変数$goods_indexの連想配列に値を代入する
+    $goods_index = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+
+    // 変数$goods_indexの連想配列の値を出力する
+    print_r($goods_index);
+    
+    ?>
+
+</body>
+
+ </html>
